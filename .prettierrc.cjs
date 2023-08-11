512a63e73f862931f2461b08d8011f785986127e
Marco Antonio Humberto Decormis Pinto <nesdufrik@gmail.com>
2023-08-11 19:40:10 -0400

    [UPDATE] formatting prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+	semi: true,
+	singleQuote: false,
+	tabWidth: 2,
+	useTabs: true,
+
+	plugins: [require.resolve("prettier-plugin-astro")],
+	overrides: [
+		{
+			files: "**/*astro",
+			options: {
+				parser: "astro",
+			},
+		},
+	],
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
-    content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
-    theme: {
-        extends: {}
-    },
-    plugins: []
-}
+	content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+	theme: {
+		extends: {},
+	},
+	plugins: [],
+};
